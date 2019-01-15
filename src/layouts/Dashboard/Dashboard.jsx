6d813c3bd64ac1c0940b235aa0ad9abc5ae2aed3
Jack Burns <johncburns1@gmail.com>
2019-01-15 13:42:21 -0800

    add logo and new text for sidebar header

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ import dashboardRoutes from "routes/dashboard.jsx";
 import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx";
 
 import image from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
+import logo from "assets/img/loopie_logo.png";
 
 const switchRoutes = (
   <Switch>
@@ -71,7 +71,7 @@ class App extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={dashboardRoutes}
-          logoText={"Creative Tim"}
+          logoText={"Loopie LLC"}
           logo={logo}
           image={image}
           handleDrawerToggle={this.handleDrawerToggle}
