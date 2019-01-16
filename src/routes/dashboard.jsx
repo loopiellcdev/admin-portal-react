b7753ddcecc29f931c2096816478171af7de5271
granth7 <gah516@gmail.com>
2019-01-16 13:33:18 -0800

    Comment unused imports

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -2,17 +2,17 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 // import ContentPaste from "@material-ui/icons/ContentPaste";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
+//import LibraryBooks from "@material-ui/icons/LibraryBooks";
+//import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
+//import Notifications from "@material-ui/icons/Notifications";
+//import Unarchive from "@material-ui/icons/Unarchive";
 // core components/views
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
-import UserProfile from "views/UserProfile/UserProfile.jsx";
-import TableList from "views/TableList/TableList.jsx";
-import Typography from "views/Typography/Typography.jsx";
-import Icons from "views/Icons/Icons.jsx";
+//import UserProfile from "views/UserProfile/UserProfile.jsx";
+//import TableList from "views/TableList/TableList.jsx";
+//import Typography from "views/Typography/Typography.jsx";
+//import Icons from "views/Icons/Icons.jsx";
 import Maps from "views/Maps/Maps.jsx";
 import Users from "views/Users/Users.jsx";
 import Customers from "views/Customers/Customers.jsx";
@@ -32,6 +32,7 @@ const dashboardRoutes = [
     icon: Dashboard,
     component: DashboardPage
   },
+
   {
     path: "/users",
     sidebarName: "Users",
